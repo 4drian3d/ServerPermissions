e4d56dbb130aadb60524b2334ffcde00642e98bf
Adrian <adriangonzalesval@gmail.com>
2023-10-18 23:05:06 -0500

    perf: Some microptimizations to the server connection listener

--- src/main/java/io/github/_4drian3d/serverpermissions/listener/ServerListener.java
+++ src/main/java/io/github/_4drian3d/serverpermissions/listener/ServerListener.java
@@ -5,21 +5,35 @@ import com.velocitypowered.api.event.*;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.plugin.PluginManager;
 import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
 import io.github._4drian3d.serverpermissions.ServerPermissions;
 import io.github.miniplaceholders.api.MiniPlaceholders;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.minimessage.Context;
+import net.kyori.adventure.text.minimessage.ParsingException;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
 
 public final class ServerListener implements AwaitingEventExecutor<ServerPreConnectEvent> {
+    private final ServerPermissions plugin;
+    private final EventManager eventManager;
+    private final boolean hasMiniPlaceholders;
+
     @Inject
-    private ServerPermissions plugin;
-    @Inject
-    private PluginManager pluginManager;
-    @Inject
-    private EventManager eventManager;
+    public ServerListener(
+            final ServerPermissions plugin,
+            final PluginManager pluginManager,
+            final EventManager eventManager
+    ) {
+        this.plugin = plugin;
+        this.eventManager = eventManager;
+        this.hasMiniPlaceholders = pluginManager.isLoaded("miniplaceholders");
+    }
 
     public void register() {
         this.eventManager.register(plugin, ServerPreConnectEvent.class, this);
@@ -27,50 +41,66 @@ public final class ServerListener implements AwaitingEventExecutor<ServerPreConn
 
     @Override
     public EventTask executeAsync(final ServerPreConnectEvent event) {
-        return EventTask.withContinuation(continuation -> {
+        return EventTask.async(() -> {
             if (!event.getResult().isAllowed()) {
-                continuation.resume();
                 return;
             }
 
-            event.getResult().getServer().ifPresent(server -> {
-                final Player player = event.getPlayer();
-                final String serverName = server.getServerInfo().getName();
+            // The check is already done in the #isAllowed() method
+            // noinspection OptionalGetWithoutIsPresent
+            final RegisteredServer server = event.getResult().getServer().get();
+            final Player player = event.getPlayer();
+            final String serverName = server.getServerInfo().getName();
 
-                // Permission Check
-                if (player.hasPermission("serverpermissions.server." + serverName)) {
-                    continuation.resume();
-                    return;
-                }
+            // Permission Check
+            if (player.hasPermission("serverpermissions.server." + serverName)) {
+                return;
+            }
 
-                event.setResult(ServerPreConnectEvent.ServerResult.denied());
+            // If the player does not have permission, access to the server is denied
+            event.setResult(ServerPreConnectEvent.ServerResult.denied());
 
-                // In case the server to which the player is connecting is the initial one,
-                // it is not necessary to send the message as the player will not see it
-                if (event.getPreviousServer() == null) {
-                    continuation.resume();
-                    return;
-                }
+            // In case the server to which the player is connecting is the initial one,
+            // it is not necessary to send the message as the player will not see it
+            if (event.getPreviousServer() == null) {
+                return;
+            }
 
-                final String noPermissionMessage = plugin.configuration().noPermissionMessage();
-                // If the message is empty, it avoids sending to the player
-                if (noPermissionMessage.isBlank()) {
-                    continuation.resume();
-                    return;
-                }
+            final String noPermissionMessage = plugin.configuration().noPermissionMessage();
+            // If the message is empty, it avoids sending to the player
+            if (noPermissionMessage.isBlank()) {
+                return;
+            }
 
-                final TagResolver.Builder builder = TagResolver.builder()
-                        .resolver(Placeholder.unparsed("server", serverName));
+            final TagResolver.Builder builder = TagResolver.builder()
+                    .resolver(new ServerResolver(serverName));
 
-                // MiniPlaceholders custom placeholders support
-                if (pluginManager.isLoaded("miniplaceholders")) {
-                    builder.resolver(MiniPlaceholders.getAudienceGlobalPlaceholders(player));
-                }
+            // MiniPlaceholders custom placeholders support
+            if (hasMiniPlaceholders) {
+                builder.resolver(MiniPlaceholders.getAudienceGlobalPlaceholders(player));
+            }
 
-                final Component message = miniMessage().deserialize(noPermissionMessage, builder.build());
-                player.sendMessage(message);
-                continuation.resume();
-            });
+            final Component message = miniMessage().deserialize(noPermissionMessage, builder.build());
+            player.sendMessage(message);
         });
+    }
+
+    private record ServerResolver(String server) implements TagResolver {
+        @Override
+        public @Nullable Tag resolve(
+                final @NotNull String name,
+                final @NotNull ArgumentQueue arguments,
+                final @NotNull Context ctx
+        ) throws ParsingException {
+            if (name.equalsIgnoreCase("server")) {
+                return Tag.preProcessParsed(server);
+            }
+            return null;
+        }
+
+        @Override
+        public boolean has(final @NotNull String name) {
+            return name.equalsIgnoreCase("server");
+        }
     }
 }
