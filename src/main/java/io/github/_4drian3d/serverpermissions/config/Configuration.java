b0e2033f1a185447affedd7d93d6ee0f6c513e97
Adrian Gonzales <adriangonzalesval@gmail.com>
2026-09-07 21:54:15 -0500

    refactor: simplify Configuration interface to record type

--- src/main/java/io/github/_4drian3d/serverpermissions/config/Configuration.java
+++ src/main/java/io/github/_4drian3d/serverpermissions/config/Configuration.java
@@ -8,45 +8,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
-public interface Configuration {
-    String noPermissionMessage();
+public record Configuration(
+    String noPermissionMessage,
+    boolean shouldLogConnectionsAttempts
+) {
 
-    boolean shouldLogConnectionsAttempts();
+  public static Configuration loadConfig(final Path path) throws IOException {
+    final Path configPath = loadFiles(path);
+    final HoconConfigurationLoader loader = HoconConfigurationLoader.builder()
+        .path(configPath)
+        .build();
 
-    static Configuration loadConfig(final Path path) throws IOException {
-        final Path configPath = loadFiles(path);
-        final HoconConfigurationLoader loader = HoconConfigurationLoader.builder()
-                .path(configPath)
-                .build();
+    final CommentedConfigurationNode loaded = loader.load();
 
-        final CommentedConfigurationNode loaded = loader.load();
+    return new Configuration(
+        loaded.node("no-permission-message").getString(""),
+        loaded.node("should-log-connections-attempts").getBoolean(false)
+    );
+  }
 
-        final String noPermissionMessage = loaded.node("no-permission-message").getString("");
-        final boolean shouldLogConnectionsAttempts = loaded.node("should-log-connections-attempts").getBoolean(false);
-
-        return new Configuration() {
-            @Override
-            public String noPermissionMessage() {
-                return noPermissionMessage;
-            }
-
-            @Override
-            public boolean shouldLogConnectionsAttempts() {
-                return shouldLogConnectionsAttempts;
-            }
-        };
+  private static Path loadFiles(final Path path) throws IOException {
+    if (Files.notExists(path)) {
+      Files.createDirectory(path);
     }
-
-    private static Path loadFiles(final Path path) throws IOException {
-        if (Files.notExists(path)) {
-            Files.createDirectory(path);
-        }
-        final Path configPath = path.resolve("config.conf");
-        if (Files.notExists(configPath)) {
-            try (final var stream = Configuration.class.getClassLoader().getResourceAsStream("config.conf")) {
-                Files.copy(Objects.requireNonNull(stream), configPath);
-            }
-        }
-        return configPath;
+    final Path configPath = path.resolve("config.conf");
+    if (Files.notExists(configPath)) {
+      try (final var stream = Configuration.class.getClassLoader().getResourceAsStream("config.conf")) {
+        Files.copy(Objects.requireNonNull(stream), configPath);
+      }
     }
+    return configPath;
+  }
 }
